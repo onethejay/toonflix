10e52a679fe90a50df40d17d4dcf9bc83c069fa3
onethejay <jhjang951@gmail.com>
2023-09-17 22:48:27 +0900

    Hello world~~

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,22 @@ void main() {
 }
 
 class App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     // MaterialApp > Google
     // CupertinoApp > Apple
     // Material 스타일이 좀 더 작업하기 좋다.
     // 물론 수정이 가능하므로 크게 신경쓰지 않아도 된다.
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        //항상 scaffold를 가지고 있어야 한다.
+        appBar: AppBar(
+          title: Text("Hello flutter!"),
+        ),
+        body: Center(
+          child: Text("Hello world!"),
+        ),
+      ),
+    );
   }
-
 }
