4d86d9c4bac2ab00e20d0deb6a62dc7f6106d24d
onethejay <jhjang951@gmail.com>
2023-09-17 23:10:19 +0900

    Header 추가

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,43 @@ class App extends StatelessWidget {
     // Material 스타일이 좀 더 작업하기 좋다.
     // 물론 수정이 가능하므로 크게 신경쓰지 않아도 된다.
     return MaterialApp(
+      //home은 항상 scaffold를 가지고 있어야 한다.
       home: Scaffold(
-        //항상 scaffold를 가지고 있어야 한다.
-        appBar: AppBar(
-          title: Text("Hello flutter!"),
-        ),
-        body: Center(
-          child: Text("Hello world!"),
-        ),
-      ),
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Hey, Selena",
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 18),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
