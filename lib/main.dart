6f0b8da67bc22c90944bb6c417e973a5f97afbaf
onethejay <jhjang951@gmail.com>
2023-09-18 22:46:02 +0900

    card, transform 실습

--- lib/main.dart
+++ lib/main.dart
@@ -117,6 +117,7 @@ class App extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
+                  clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                     color: const Color(0xFF1f2123),
                     borderRadius: BorderRadius.circular(25),
@@ -124,6 +125,7 @@ class App extends StatelessWidget {
                   child: Padding(
                     padding: EdgeInsets.all(30),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -161,6 +163,17 @@ class App extends StatelessWidget {
                               ],
                             ),
                           ],
+                        ),
+                        Transform.scale(
+                          scale: 2.2,
+                          child: Transform.translate(
+                            offset: Offset(-5, 12),
+                            child: Icon(
+                              Icons.euro_rounded,
+                              color: Colors.white,
+                              size: 88,
+                            ),
+                          ),
                         ),
                       ],
                     ),
