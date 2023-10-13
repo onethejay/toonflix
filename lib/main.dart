04a3635accead007be3e3aca0e3d6edb6b81e65c
onethejay <jhjang951@gmail.com>
2023-10-13 23:24:20 +0900

    widget life cycle 실습!

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,7 +36,8 @@ class _AppState extends State<App> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? MyLargeTitle() : Text("Nothing"),
+              IconButton(onPressed: toggleTitle, icon: Icon(Icons.remove_red_eye_rounded))
             ],
           ),
         ),
@@ -37,13 +46,39 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  void initState() {
+    //항상 build 메서드보다 먼저 호출되어야 한다.
+    //항상 1회만 호출된다.
+    super.initState();
+    //항상 super.initState()를 작성하고 이후에 코드를 추가해야한다.
+    print("initState!");
+  }
+
+  @override
+  void dispose() {
+    //위젯이 스크린에서 제거될 때 호출되는 메서드
+    //API 업데이트나 이벤트 리스너로부터 구독을 취소하거나
+    //form의 리스너로부터 해제할 때 사용한다.
+    super.dispose();
+    print("dispose!");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build!");
     //context를 통해 앱의 위젯 트리에 접근할 수 있다.
     return Text(
       "My Large Title",
