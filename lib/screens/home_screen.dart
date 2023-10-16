f2df60481c3ecdc995bd99b2d2dd427820c04a2e
onethejay <jhjang951@gmail.com>
2023-10-16 22:49:52 +0900

    Pause / Play 실습

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = 1500;
+  bool isRunning = false;
   late Timer timer; //당장 초기화 하지 않아도 되지만 사용하기 전에 반드시 초기화해야 한다.
 
   void onTick(Timer timer) {
@@ -24,6 +25,16 @@ class _HomeScreenState extends State<HomeScreen> {
       Duration(seconds: 1),
       onTick,
     ); //괄호를 넣으면 여기에서 실행한다는 의미이므로 괄호를 붙이지 않아야 한다.
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -52,8 +63,8 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: onStartPressed,
-                icon: Icon(Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                icon: Icon(isRunning ? Icons.pause_circle_outline : Icons.play_circle_outline),
               ),
             ),
           ),
